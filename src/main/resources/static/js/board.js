39859250df580af312aef1b622d84fcc0ad60835
gmun <gmun0929@gmail.com>
2019-07-05 22:40:57 +0900

    게시판 뷰(html,css,js) 개발

---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+	getBoardList();
+});
+
+
+function factoryBoards(){
+	return {
+		 setBoards : function(_boardList){
+			boardList = _boardList;
+		 }
+	
+		,getBoards : function(){
+			return boardList;
+		}
+	}
+}
+
+function getBoardList(pageNum){
+	var pageNum = pageNum || 1;
+	var keyword = $("#keyword").val() || "";
+	
+	$.ajax({
+		 type: "GET"
+		,url: "/api/board/"+pageNum+"?type=1&keyword="+keyword
+		,success: function (resObj) {
+			new factoryBoards().setBoards(resObj);
+			makeHtmlCode(resObj.data);
+		}
+	});
+}
+
+function deleteBoard(id){
+	if(!confirm("삭제하시겠습니까?")){
+		return;
+	}
+	$.ajax({
+		 type: "DELETE"
+		,contentType: "application/json"
+		,url: "/api/board/" + id
+		,success: function (resObj) {
+			if(resObj == "SUCCESS"){
+				getBoardList();
+			}
+			$("#myModal").modal("hide");
+		}
+		,error: function (resObj) {}
+	});
+}
+
+function createBoard() {
+    var params = {
+    	 title 	  : $("#title").val()
+    	,contents : $("#contents").val()
+    	,type	  : 1
+    }
+    $.ajax({
+         type: "POST"
+        ,contentType: "application/json"
+        ,url: "/api/board/"
+        ,dataType: 'json'
+        ,data: JSON.stringify(params)
+        ,success: function (resObj) {
+        	getBoardList();
+        	$("#myModal").modal("hide");
+        }
+        ,error: function (resObj) {}
+    });
+}
+
+function updateBoard(id) {
+	var params = {
+			 title 	  : $("#title").val()
+			,contents : $("#contents").val()
+			,type	  : 1
+	}
+	$.ajax({
+		//type: "PUT"
+		type: "PATCH"
+	   ,contentType: "application/json"
+	   ,url: "/api/board/"+id
+	   ,dataType: 'json'
+	   ,data: JSON.stringify(params)
+	   ,success: function (resObj) {
+			getBoardList();
+			$("#myModal").modal("hide");
+		}
+	,error: function (resObj) { }
+	});
+}
---
+++ src/main/resources/static/js/template.js
@@ -0,0 +1,148 @@
+$(document).ready(function () {
+	eventManagement();
+});
+
+function eventManagement(){
+	$("#myBtn").click(function(){
+		openModal();
+    });
+	
+	$("#keyword")
+	.keydown(function(e){
+		if(e.keyCode == 13){
+			getBoardList();
+		}
+	});
+	$("#keywordBtn")
+	.click(function(e){
+		getBoardList();
+	});
+}
+
+function makeHtmlCode(params){
+	makeTableHtmlCode(params.content || []);
+	makePageNationHtmlCode(params);
+	
+	function makeTableHtmlCode(resRowDatas){
+		var $selector = $("#noticeContents");
+		var htmlCode = "";
+		if(resRowDatas.length > 0){
+			for(var rowIdx in resRowDatas){
+				var id 		 	 = resRowDatas[rowIdx].id;
+				var title 		 = resRowDatas[rowIdx].title || "";
+				var name 	 	 = resRowDatas[rowIdx].name || "";
+				var createdDate  = resRowDatas[rowIdx].createdDate.substr(0,10) || "";
+				var modifiedDate = resRowDatas[rowIdx].modifiedDate.substr(0,10) || "";
+				var contents 	 = resRowDatas[rowIdx].contents.replace(/(?:\r\n|\r|\n)/g, '<br />') || "";
+				
+				htmlCode +="<tr onclick='triggerContents("+id+")'>"
+						  +"<td>"+ title +"</td>" 
+						  +"<td>"+ name +"</td>" 
+						  +"<td>"+ createdDate +"</td>" 
+						  +"<td>"+ modifiedDate +"</td>"
+						  +"</tr>" 
+						  
+						  +"<tr style='display:none;' id='contents"+id+"' class='contents'>"
+						  +"<td colspan='4' style='padding:3%;'>"+ contents +"</td>"
+						  
+						  +"<tr style='display:none;' id='btnArea"+id+"' class='contents'>"
+						  +"<td colspan='4' style='text-align:right;'>"
+						  +"<button class=\"btn btn-info\" onclick=\"openModal("+id+");\">수정</button>" 
+						  +"<button class=\"btn btn-danger\" onclick=\"deleteBoard("+id+");\">삭제</button>" 
+						  +"</td>"
+						  
+						  +"</tr>" ;
+			}
+		}else{
+			htmlCode +="<tr>"
+					  +"<td colspan='4' align='center'>'"+ $("#keyword").val() + "'에 대한 검색결과가 없습니다.</td>"
+				 	  +"</tr>";
+		}
+		$selector.html(htmlCode);
+	}
+	
+	function makePageNationHtmlCode(params){
+		var $selector = $(".baordPaging>.pagination");
+		var htmlCode = "";
+		
+		if(!params.empty){
+			var pageable = params.pageable;
+			
+			// 페이지 제한 블록
+			var limitBlock = 5;
+			// 전체 페이지 수
+			var totalPages = params.totalPages;
+			// 전체 페이지 블록 수
+			var totaBlcok = totalPages%limitBlock == 0 ? totalPages/limitBlock : totalPages/limitBlock + 1;
+			// 현재 페이지 수
+			var curPageNum = pageable.pageNumber+1; // jpa 0부터 시작
+			// 현재 블록
+			var curBlock = Math.ceil(curPageNum/limitBlock); // 올림
+			// 블록 시작을 알리는 숫자
+			var firstBlockNum = (curBlock-1)*limitBlock + 1;
+			// 블록 마지막을 알리는 숫자
+			var lastBlockNum = (firstBlockNum + limitBlock) > totalPages ? totalPages + 1: (firstBlockNum + limitBlock);
+			
+			
+			var isDisableLeftArrow = (curBlock == 1 || params.first) ? ' class="disabled" ' : ' onClick=\"getBoardList('+ (firstBlockNum-1) +')\"';
+			htmlCode += "<li" + isDisableLeftArrow + ">"
+					 + '<a href=\"javascript:void(0)\"> <i class=\"fa fa-chevron-left\"></i></a>'
+					 + "</li>";
+			
+			for(var i = firstBlockNum; i < lastBlockNum; i++){
+				var active = curPageNum == i ? " class=' active'" : ' onClick=\"getBoardList('+ i +')\"';
+				htmlCode +="<li"+active+">"
+						 +'<a href=\"javascript:void(0)\">' + (i) + '</a>'
+						 +"</li>";
+			}
+			
+			var isDisableRightArrow = (totaBlcok == curBlock || params.last) ? ' class="disabled" ' : ' onClick=\"getBoardList('+ (lastBlockNum) +')\"';
+			htmlCode += "<li" + isDisableRightArrow + ">"
+					 + '<a href=\"javascript:void(0)\"> <i class=\"fa fa-chevron-right\"></i> </a>'
+					 + "</li>";
+		}
+		$selector.html(htmlCode);
+	}
+}
+
+function triggerContents(id){
+	var $selector = $("#contents"+id+", #btnArea"+id);
+	
+	if($selector.css("display")=="none"){
+		$selector.css("display","table-row");
+	}else{
+		$selector.css("display","none");
+		
+	}
+}
+
+function openModal(id){
+	var id = id || "";
+	var $modalTitle = $("#modalTitle");
+	var $btnModal 	= $("#btnModal");
+
+	if(id == ""){
+		$modalTitle.text("글 작성");
+		$btnModal.text("작성");
+		$(".modal-footer").html('<button class="btn btn-primary" id="btnModal" onclick="createBoard();" data-dismiss="modal">저장</button>');
+		$("#title").val("");
+		$("#contents").val("");
+		
+	}else{
+		$modalTitle.text("글 수정");
+		$btnModal.text("수정");
+		$(".modal-footer").html('<button class="btn btn-primary" id="btnModal" onclick="updateBoard('+id+');" data-dismiss="modal">수정</button>');
+		
+		var boards = new factoryBoards().getBoards().data.content;
+		for(idx in boards){
+			var board = boards[idx];
+			if(id==board.id){
+				$("#title").val(board.title);
+				$("#contents").val(board.contents);
+				break;
+			}
+			continue;
+		}
+	}
+	$("#myModal").modal();
+}
