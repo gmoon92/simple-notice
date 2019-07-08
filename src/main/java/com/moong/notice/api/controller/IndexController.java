0f953627199aaaa8046e810a817e86ecf3433f1f
gmun <gmun0929@gmail.com>
2019-07-09 04:07:14 +0900

    session 로그인 강제 추가

--- src/main/java/com/moong/notice/api/controller/IndexController.java
+++ src/main/java/com/moong/notice/api/controller/IndexController.java
@@ -1,13 +1,27 @@
 package com.moong.notice.api.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.moong.notice.domain.member.Member;
+import com.moong.notice.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+	private final MemberRepository memberRepository;
 	
-	@GetMapping(value={"", "/index", "/home"})
-	public String index() {
-		return "index";
+	@RequestMapping(value= {"/", "/home", "index"})
+	public String home(HttpServletRequest request) {
+		Member loginUser = memberRepository.findByUId("moong");
+		HttpSession session = request.getSession(true);
+		session.setAttribute("loginUser", loginUser);
+		return "/notice";
 	}
+
 }
