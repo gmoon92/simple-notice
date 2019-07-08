1573aed6d3f221cc50358a6d1e1102fed7d9f9e9
gmun <gmun0929@gmail.com>
2019-07-09 04:15:24 +0900

    WebMvconfig, CommandLineRunner 설정 추가

--- src/main/java/com/moong/notice/NoticeApplication.java
+++ src/main/java/com/moong/notice/NoticeApplication.java
@@ -1,18 +1,7 @@
 package com.moong.notice;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import com.moong.notice.domain.board.Board;
-import com.moong.notice.domain.board.BoardType;
-import com.moong.notice.repository.BoardRepository;
-
-import lombok.RequiredArgsConstructor;
 
 /**
 [Backend 사전과제]
@@ -33,31 +22,10 @@ import lombok.RequiredArgsConstructor;
  * 제출시 프로젝트 빌드, 실행 방법 명시 (ex.README.md 파일에 설명을 기술)
  *  
 ### 과제 요구사항
- 
  **/
 @SpringBootApplication
-@RequiredArgsConstructor
-@EnableJpaAuditing
-public class NoticeApplication implements CommandLineRunner{
-
-	private final BoardRepository boardRepository;
-	
+public class NoticeApplication { 
 	public static void main(String[] args) {
 		SpringApplication.run(NoticeApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		List<Board> boards = new ArrayList<Board>();
-		for(int i=0; i<30; i++) {
-			boards.add(
-					Board.builder()
-						 .title("제목" + i)
-						 .contents(new StringBuffer().append(i))
-						 .type(BoardType.NOTICE)
-						 .build()
-					);
-		}
-		boardRepository.saveAll(boards);
 	}
 }
---
+++ src/main/java/com/moong/notice/config/JPAConfig.java
@@ -0,0 +1,47 @@
+package com.moong.notice.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.moong.notice.domain.board.Board;
+import com.moong.notice.domain.board.BoardType;
+import com.moong.notice.domain.member.Member;
+import com.moong.notice.domain.member.MemberRules;
+import com.moong.notice.repository.BoardRepository;
+import com.moong.notice.repository.MemberRepository;
+
+@EnableJpaAuditing
+@Configuration
+public class JPAConfig {
+
+	@Bean
+	public CommandLineRunner setUp(BoardRepository boardRepository
+								,MemberRepository memberRepository){
+		return (args) -> {
+			Member loginMember = memberRepository.save(Member.builder()
+															 .uId("moong")
+															 .uPw("1234")
+															 .rule(MemberRules.ADMIN)
+															 .name("문겸")
+															 .build());
+			
+			List<Board> boards = new ArrayList<Board>();
+		
+			for(int i=1; i<13; i++) {
+				Board savedBoard = Board.builder()
+										.title("제목"+i)
+										.contents(new StringBuffer().append("내용"))
+										.type(BoardType.NOTICE)
+										.build()
+										.setMember(loginMember);
+				boards.add(savedBoard);
+			}
+			boardRepository.saveAll(boards);
+		};
+	}
+}
