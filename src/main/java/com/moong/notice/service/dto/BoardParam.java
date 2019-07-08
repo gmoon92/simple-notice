5b2701215dece57f57a340b7c63d3bbc70744a6c
gmun <gmun0929@gmail.com>
2019-07-09 04:11:48 +0900

    게시판 리퀘스트 파라미터 추가

---
+++ src/main/java/com/moong/notice/service/dto/BoardParam.java
@@ -0,0 +1,53 @@
+package com.moong.notice.service.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.moong.notice.domain.board.Board;
+import com.moong.notice.domain.board.BoardType;
+import com.moong.notice.domain.member.Member;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+@NoArgsConstructor
+public class BoardParam {
+	
+	@NotEmpty // null, "" 허용 x,  " " 허용
+	private String title;
+	
+	@NotEmpty // null, "" 허용 x,  " " 허용
+	private StringBuffer contents;
+	
+	@NotNull // null 허용 x, "", " " 허용
+	private BoardType type;
+	
+	@NotBlank // null, "", " " 허용 x
+	private String u_id;
+	
+	private Member member;
+	
+	@Builder
+	public BoardParam(String title, StringBuffer contents, BoardType type, String u_id, Member member) {
+		this.title = title;
+		this.contents = contents;
+		this.type = type;
+		this.u_id = u_id;
+		this.member = member;
+	}
+	
+	public Board toEntity(){
+		Board board = Board.builder()
+						   .title(title)
+						   .type(type)
+						   .contents(contents)
+						   .build()
+						   .setMember(member);
+		return board;
+	}
+}
